c54ab2449b96e485d44961d115b5e0e79a38434f
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-10 14:12:35 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
 import Footer from "./components/Footer";
-import HeroInteractive from "./components/HeroInteractive"; // 👈 yeni bileşeni dahil ettik
+import HeroInteractive from "./components/HeroInteractive";
 import HeroDemo from "./components/HeroDemo";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Auth from "./components/Auth";
