35c4a87cee67f82705864d9152c651633629b2bb
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-10 14:18:07 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "../assets/logo.png";
 import Button from "./Button";
-import { Home, User } from "lucide-react"; // 👈 User ikonu eklendi
+import { Home, User } from "lucide-react";
 
 function Header() {
   return (
