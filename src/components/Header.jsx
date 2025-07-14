1089fe808295e85acb34d8454add6083590c4c5a
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-15 02:44:22 +0300

    delete AlertManager button from landpage

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import logo from "../assets/logo.png";
 import Button from "./Button";
 import { Home, User } from "lucide-react";
-import AlertManager from "./AlertManager";
-
 
 function Header() {
   return (
@@ -16,10 +14,6 @@ function Header() {
         <Button onClick={() => window.location.href = '/'}>
           <Home className="w-6 h-6 text-[#3CB97F]" />
         </Button>
-
-        {/* Bildirim butonu  ← EKLENDİ */}
-        <AlertManager />
-
 
         {/* Giriş / Kayıt tek ikonlu buton */}
         <Button onClick={() => window.location.href = '/auth'}>
