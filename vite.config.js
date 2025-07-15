6b43fa3923d5e46319b6144435b05637c4c8af56
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-15 16:37:57 +0300

    update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
  
 export default defineConfig({
   plugins: [react()],
-  base: '/frontend/',
+  base: process.env.NODE_ENV === 'production' ? '/frontend/' : '/',
 }) 
