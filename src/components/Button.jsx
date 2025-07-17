1f23dd9a709e9e1e7344fd71796b6875d0b8cd78
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-18 01:58:35 +0300

    Update Button.jsx: change color

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,7 @@ import React from "react";
 function Button({ children, onClick }) {
   return (
     <button
-      className="text-xl px-8 py-3 border-2 border-[#e0e7ef] text-gray-800 rounded-full bg-[#e0e7ef] hover:bg-[#d3dae6] hover:text-gray-800 transition font-semibold active:bg-[#c3c9d6] active:text-gray-800 active:scale-95"
+      className="text-xl px-8 py-3 border-2 border-[#e0e7ef] text-gray-800 rounded-full bg-[#5e5e5e] hover:bg-[#d3dae6] hover:text-gray-800 transition font-semibold active:bg-[#c3c9d6] active:text-gray-800 active:scale-95"
       onClick={onClick}
     >
       {children}
