4b4b9e09ce5182f413ccf03c1366afefb5fe0540
elifarslancelik <elifnurarslnclk@gmail.com>
2025-07-17 18:24:29 +0300

    update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,17 @@ export default {
     "./index.html",
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
+  safelist: [
+    'bg-gradient-to-r',
+    'from-[#3CB97F]',
+    'via-[#1c1c1e]',
+    'to-[#3CB97F]',
+    'from-[#ececec]',
+    'to-[#e0e7ef]',
+    'from-indigo-900',
+    'to-green-700',
+    // Diğer gradient ve renk class'larınız varsa buraya ekleyin
+  ],
   theme: {
     extend: {
       fontFamily: {
