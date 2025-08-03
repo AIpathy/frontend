cf2f290b66080c06a9c9cb58280c9f40316042be
elifarslancelik <elifnurarslnclk@gmail.com>
2025-08-03 17:33:55 +0300

    bug fixes: Hero.jsx: change import path

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import MentalHealthChart from "./MentalHealthChart";
+import MentalHealthChart from "./components/MentalHealthChart";
 
 
 function Hero() {
